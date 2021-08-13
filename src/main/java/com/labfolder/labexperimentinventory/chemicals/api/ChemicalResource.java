29c807cd016176a8384c50998f60ffeb0e809812
Esupu Raphael <eraphael4@gmail.com>
2021-08-13 15:58:54 +0300

    - adds chemicals resource implementation

--- src/main/java/com/labfolder/labexperimentinventory/chemicals/api/ChemicalResource.java
+++ src/main/java/com/labfolder/labexperimentinventory/chemicals/api/ChemicalResource.java
@@ -1,10 +1,41 @@
 package com.labfolder.labexperimentinventory.chemicals.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.labfolder.labexperimentinventory.chemicals.models.Chemical;
+import com.labfolder.labexperimentinventory.chemicals.models.CreateChemicalRequest;
+import com.labfolder.labexperimentinventory.chemicals.service.ChemicalService;
+import com.labfolder.labexperimentinventory.utils.wrappers.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = ChemicalResource.CHEMICAL_RESOURCE_PATH)
 public class ChemicalResource {
     public static final String CHEMICAL_RESOURCE_PATH = "/apis/chemicals";
+
+    @Autowired
+    private ChemicalService chemicalService;
+
+    @PostMapping
+    public ResponseEntity<Id> createChemical(@RequestBody CreateChemicalRequest chemicalRequest) {
+        return ResponseEntity.ok(new Id(chemicalService.addChemical(chemicalRequest)));
+    }
+
+    @GetMapping("/{chemicalId}")
+    public ResponseEntity<Optional<Chemical>> getChemicalById(@PathVariable("chemicalId") Long chemicalId) {
+        return ResponseEntity.ok(chemicalService.getChecmicalById(chemicalId));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Chemical> updateChemical(@RequestBody CreateChemicalRequest chemicalRequest) {
+        return ResponseEntity.ok().body(chemicalService.updateChemical(chemicalRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Chemical>> getAllChemicals(){
+        return ResponseEntity.of(chemicalService.getAllChemicals());
+    }
 }
--- src/main/java/com/labfolder/labexperimentinventory/chemicals/service/ChemicalService.java
+++ src/main/java/com/labfolder/labexperimentinventory/chemicals/service/ChemicalService.java
@@ -1,8 +1,13 @@
 package com.labfolder.labexperimentinventory.chemicals.service;
 
+import com.labfolder.labexperimentinventory.chemicals.models.Chemical;
+import com.labfolder.labexperimentinventory.chemicals.models.CreateChemicalRequest;
 import com.labfolder.labexperimentinventory.chemicals.store.ChemicalStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ChemicalService {
@@ -10,5 +15,19 @@ public class ChemicalService {
     @Autowired
     private ChemicalStore chemicalStore;
 
+    public Long addChemical(CreateChemicalRequest chemicalRequest) {
+        return chemicalStore.saveChemical(chemicalRequest.asChemical());
+    }
 
+    public Chemical updateChemical(CreateChemicalRequest chemical) {
+        return chemicalStore.updateChemical(chemical.asChemicalUpdate());
+    }
+
+    public Optional<Chemical> getChecmicalById(Long chemicalId) {
+        return chemicalStore.getChemicalById(chemicalId);
+    }
+
+    public Optional<List<Chemical>> getAllChemicals() {
+        return chemicalStore.getAllChemicals();
+    }
 }
